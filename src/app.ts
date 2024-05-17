b2e810a1a1fa236a174777415df4c92c41c6ce41
arpapon <ashfaqurrahman377@gmail.com>
2024-05-18 01:45:27 +0600

    changes

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,21 @@
 import { Application, Request, Response } from 'express';
 import cors from 'cors';
 import express from 'express';
+import { StudentRoutes } from './app/config/modules/student/student_route';
 const app: Application = express();
 
 //parsers
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  let a = 10;
+//application route
+app.use('/api/v1/students', StudentRoutes);
 
+const getAController = (req: Request, res: Response) => {
+  let a = 10;
   res.send(a);
-});
+};
+
+app.get('/', getAController);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ import mongoose from 'mongoose';
 
 async function main() {
   try {
-    await mongoose.connect(config.database as string);
+    await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(` app listening on port ${config.port}`);
     });
   } catch (err) {
     console.log(err);
